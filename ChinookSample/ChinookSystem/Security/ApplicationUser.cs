a35b28f6e7cc3d9b96847afdd1efc33275cd9abc
Don Welch <dwelch@nait.ca>
2016-10-13 16:40:33 -0600

    Add additional tracking properties for ApplicationUser
    fields can act as foriegn key values into the user data

--- ChinookSample/ChinookSystem/Security/ApplicationUser.cs
+++ ChinookSample/ChinookSystem/Security/ApplicationUser.cs
@@ -9,9 +9,12 @@ using Microsoft.AspNet.Identity.EntityFramework;    //IdentityUser
 #endregion
 namespace ChinookSystem.Security
 {
-    // You can add User data for the user by adding more properties to your User class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+    // You can add User data for the user by adding more properties 
+    //to your User class, 
+    //please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
-        
+        public int? EmployeeId { get; set; }
+        public int? CustomerId { get; set; }
     }
 }
--- ChinookSample/ChinookSystem/Security/RoleManager.cs
+++ ChinookSample/ChinookSystem/Security/RoleManager.cs
@@ -4,9 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+#region Additional Namespaces
+using Microsoft.AspNet.Identity;                    //RoleManger
+using Microsoft.AspNet.Identity.EntityFramework;    //IdentityRole, RoleStore
+#endregion
 namespace ChinookSystem.Security
 {
-    class RoleManager
+    public class RoleManager: RoleManager<IdentityRole>
     {
+        public RoleManager():
+            base (new RoleStore<IdentityRole>(new ApplicationDbContext()))
+        {
+
+        }
     }
 }
